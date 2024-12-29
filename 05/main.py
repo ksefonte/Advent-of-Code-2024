dd34f765463b156cf53b75e5a2260ea20947ab18
ksefonte <sefonte.kyle@gmail.com>
2024-12-29 23:46:31 +1300

    Day 05 Pt 2

--- 05/main.py
+++ 05/main.py
@@ -38,52 +38,102 @@ def classify_rules(rules: list):
     return list_of_rules
 
 def process_updates(updates: list):
-    print("Raw Updates:",updates)
+    enable_logs = False
+    if enable_logs: print("Raw Updates:",updates)
     total_valid_update_sets = 0
     sum_of_middle_numbers = 0
-    list_of_updates = []
+    ordered_updates = []
+    unordered_updates = []
     for update in updates:
         update = update.split(',')
         update_length = len(update)
-        print("Length is:",update_length,"update is:",update)
         middle_update = int(update[int((update_length-1)/2)])
-        print("middle number is:",middle_update,type(middle_update))
-        print("Update:",update)
+        if enable_logs:
+            print("Length is:",update_length,"update is:",update)
+            print("middle number is:",middle_update,type(middle_update))
+            print("Update:",update)
         valid_updates = 0
         for i in range(update_length):
-            print(len(update))
-            # befores = Rule.find_by_before(update[i])
-            print("I is:",i)
-            print("Update is:",update)
+            if enable_logs:
+                print("I is:",i)
+                print("Update is:",update)
             if validate_page_position(i,update):
                 valid_updates += 1
-        print("V,V",valid_updates,len(update))
+        if enable_logs:
+            print("V,V",valid_updates,len(update))
         if valid_updates == len(update):
             total_valid_update_sets += 1
             sum_of_middle_numbers += middle_update
+            ordered_updates.append(update)
+        else:
+            unordered_updates.append(update)
+    return total_valid_update_sets, sum_of_middle_numbers, ordered_updates, unordered_updates
 
-    return total_valid_update_sets, sum_of_middle_numbers
+def process_unordered_updates(updates: list):
+    enable_logs = True
+    print("Raw Updates:",updates)
+    reordered_total_valid_update_sets = 0
+    reordered_sum_of_middle_numbers = 0
+    reordered_ordered_updates = []
+    unordered_updates = []
+    for update in updates:
+        ordered = False
+        update_length = len(update)
+        # while ordered == False:
+        valid_updates = 0
+        while valid_updates != update_length:
+            valid_updates = 0
+            print("Update length",update_length)
+            for i in range(update_length):
+                if enable_logs:
+                    print("I is:",i)
+                    print("Update is:",update)
+                    print('Select update is:',update[i])
+                update_ordered = validate_page_position(i,update)
+                if update_ordered:
+                    print("Valid order")
+                    valid_updates +=1
+                elif update_ordered != True:
+                    update_to_reorder = update.pop(i)
+                    print("RE:",update_to_reorder)
+                    update.insert(0,update_to_reorder)
+                    valid_updates = 0
+        if enable_logs:
+            print("V,V",valid_updates,len(update))
+        if valid_updates == len(update):
+            reordered_total_valid_update_sets += 1
+            ordered_updates.append(update)
+            middle_update = int(update[int((update_length-1)/2)])
+            reordered_sum_of_middle_numbers += middle_update
+        else:
+            unordered_updates.append(update)
+        if enable_logs:
+            print("Length is:",update_length,"update is:",update)
+            print("Update:",update)
+    return reordered_total_valid_update_sets, reordered_sum_of_middle_numbers, reordered_ordered_updates
+
 
 def validate_page_position(page_index: int, update_set: list):
-    print('Before pop:',update_set)
-    print("Index",page_index)
+    # print('Before pop:',update_set)
+    # print("Index",page_index)
     working_set = update_set.copy()
     page = working_set.pop(page_index)
-    print("Page:",page)
-    print("Update after pop:",working_set)
-    print("Lenght:",len(working_set))
+    # print("Page:",page)
+    # print("Update after pop:",working_set)
+    # print("Lenght:",len(working_set))
     rules = Rule.find_by_before(page)
     for i in range(len(working_set)):
-        print(i)
         update = working_set[i]
         for rule in rules:
             if update == rule.y and i < page_index:
                 return False
     return True
 
-
 rules = read_file('./input_rules.txt')
 updates = read_file('./input_updates.txt')
 list_of_rules = classify_rules(rules)
-total_valid_update_sets, sum_of_middle_numbers = process_updates(updates)
+total_valid_update_sets, sum_of_middle_numbers, ordered_updates, unordered_updates = process_updates(updates)
 print("Sum of middle numbers is:",sum_of_middle_numbers)
+print(len(unordered_updates))
+reordered_total_valid_update_sets, reordered_sum_of_middle_numbers, reordered_ordered_updates = process_unordered_updates(unordered_updates)
+print("Middle:",reordered_sum_of_middle_numbers)
